0b68b9aea51c1e30f2d87536a841f0fe9f7f5f41
Raj S <rajenderdangwal@gmail.com>
2020-01-14 22:50:50 +1100

    Fixed the test case.

--- src/test/java/com/learning/spring/mvc/WebEndpointTest.java
+++ src/test/java/com/learning/spring/mvc/WebEndpointTest.java
@@ -1,23 +1,33 @@
 package com.learning.spring.mvc;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
-import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.learning.spring.springlearning.SpringLearningApplication;
+
 @RunWith(SpringRunner.class)
 @WebMvcTest
-class WebEndpointTest {
+@ContextConfiguration(classes = SpringLearningApplication.class)
+public class WebEndpointTest {
 
 	@Autowired
-	  private MockMvc mockMvc;
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	private MockMvc mockMvc;
+	
+	@org.junit.Test
+	public void contextLoads() {
+	}
+	
+	@org.junit.Test
+	public void testNonAuthenticatedEP() throws Exception {
+		mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(content().string("Accessed the root URL !"));
 	}
 
 }
