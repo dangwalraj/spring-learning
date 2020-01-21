c137fc5fe4fe550556c1fa238728e6e9ec00e21b
R S <rajenderdangwal@gmail.com>
2020-01-21 16:36:28 +1100

    Updated the login page and also added margin before login  prompt.

--- src/main/java/com/learning/spring/security/BasicSecurityManager.java
+++ src/main/java/com/learning/spring/security/BasicSecurityManager.java
@@ -1,7 +1,12 @@
 package com.learning.spring.security;
 
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,6 +15,9 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @EnableWebSecurity
 @Configuration
@@ -25,7 +33,9 @@ public class BasicSecurityManager extends WebSecurityConfigurerAdapter {
 		.and()
 		.formLogin().loginPage("/login")
 		.defaultSuccessUrl("/home")
-		.failureUrl("/login?error")
+		.failureHandler(getCustomHander())
+		
+		//.failureUrl("/login?error")
 		.permitAll()
 		.and()
 		.logout().invalidateHttpSession(true)
@@ -46,6 +56,23 @@ public class BasicSecurityManager extends WebSecurityConfigurerAdapter {
 		
 		
 		return new InMemoryUserDetailsManager(user);
+	}
+	
+	@Bean
+	private AuthenticationFailureHandler getCustomHander() {
+		return (request, response, exception) ->
+		{
+			response.setStatus(HttpStatus.UNAUTHORIZED.value());
+			Map<String, Object> data = new HashMap<String, Object>();
+			data.put("timestamp", Calendar.getInstance().getTime());
+			data.put("exception",  exception.getMessage());
+			
+			String objMapperOut = new ObjectMapper().writeValueAsString(data);
+			System.out.println("Error from Login Page"+ objMapperOut);
+			response.getOutputStream().println(new ObjectMapper().writeValueAsString(data)
+					);
+		};
+		
 	}
 
 }
